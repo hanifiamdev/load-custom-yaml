432c3a5f5309c24cc51d98ce7df08a47f1290005
Hanif Amrullah <53424589+hanifiamdev@users.noreply.github.com>
2025-03-30 06:36:51 +0700

    Update Sample.java

--- src/main/java/com/yaml/load/Sample.java
+++ src/main/java/com/yaml/load/Sample.java
@@ -2,6 +2,6 @@ package com.yaml.load;
 
 public class Sample {
     public static void main(String[] args) {
-        System.out.println("hi, hp");
+        System.out.println("hi, how are you");
     }
 }
